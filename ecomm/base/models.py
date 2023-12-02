673a442d04d946f59c3029023cbf8234f34d280d
Haarish Khan <khan1281979harry@gmail.com>
2023-12-02 23:26:24 +0530

    Base helper model

--- ecomm/base/models.py
+++ ecomm/base/models.py
@@ -1,1 +1,11 @@
+import uuid
 from django.db import models
+
+
+class BaseModel(models.Model):
+    uid = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid64)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)    
+
+    class Meta:
+        abstract = True
