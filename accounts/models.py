a9faff99cc3fa3316dbf84024abe54f63655b11c
Haarish Khan <khan1281979harry@gmail.com>
2023-12-03 01:51:10 +0530

    Profile model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from base.models import BaseModel
 # Create your models here.
+
+class Profile(BaseModel):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    is_email_verified = models.BooleanField
