987a0a2da6cae4f25f4cadaab56a809af3975b08
Haarish Khan <khan1281979harry@gmail.com>
2023-12-03 03:04:40 +0530

    34.21 pause

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,8 @@
-from django.contrib import admin
+
 from django.urls import path
-from django.conf.urls.static import static
+from accounts.views import login_page
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('login/', login_page, name="login"),
 ]
