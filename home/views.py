18fd914e0ad3cec9cdd78c3e11dfa5032131c38c
Haarish Khan <khan1281979harry@gmail.com>
2023-12-06 00:52:28 +0530

    context return in views

--- home/views.py
+++ home/views.py
@@ -4,4 +4,4 @@ from products.models import Product
 
 def index(request):
     context= {'products': Product.objects.all()}
-    return render(request, 'home/index.html')
+    return render(request, 'home/index.html', context)
