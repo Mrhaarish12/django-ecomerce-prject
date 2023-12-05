d641738e0c81b149ee75b1439cc8a3988e94e13d
Haarish Khan <khan1281979harry@gmail.com>
2023-12-06 00:42:09 +0530

    models object

--- products/admin.py
+++ products/admin.py
@@ -5,4 +5,8 @@ from .models import *
 
 admin.site.register(Category)
 admin.site.register(Product)
+
+class ProductImageAdmin(admin.StackedInline):
+    models = ProductImage
+    
 admin.site.register(ProductImage)
