7096503e5018e6e375fa52f0e4fcb103dd7099bb
Haarish Khan <khan1281979harry@gmail.com>
2023-12-08 01:55:11 +0530

    str method

--- products/models.py
+++ products/models.py
@@ -20,7 +20,7 @@ class ColorVariant(BaseException):
     price = models.IntegerField(default=0)
 
     def __str__(self) -> str:
-        return
+        return self.color_name
 
 class SizeVariant(BaseModel):
     size_name = models.CharField(max_length=100)
