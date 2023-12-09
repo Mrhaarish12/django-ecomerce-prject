04bbd4a7b142174190e0effd5b820a1a12eaa28a
Haarish Khan <khan1281979harry@gmail.com>
2023-12-09 15:56:58 +0530

    context

--- products/views.py
+++ products/views.py
@@ -5,7 +5,7 @@ from products.models import Product
 def get_products(request, slug):
     try:
         product = Product.objects.get(slug=slug)
-
+        context = {'product' : product}
         if request.GET.get('size'):
             size = request.GET.get('size')
             price = product.get_product_price_by_size(size)
